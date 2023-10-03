2ee6484b1f37022c1c3a97ba31e8c21f598b33f1
clasei <clasei@proton.me>
2023-10-03 13:58:36 +0200

    animationMatter.js function created

---
+++ js/animationMatter.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const { Engine, Render, World, Bodies } = Matter;
+
+    const engine = Engine.create();
+    const render = Render.create({
+        element: document.getElementById('animation'),
+        engine: engine,
+        options: {
+            width: 600,
+            height: 400,
+            wireframes: false
+        }
+    });
+
+    const ball = Bodies.circle(300, 0, 20, {
+        density: 0.04,
+        frictionAir: 0.006,
+        restitution: 0.9,
+        friction: 0.01,
+        render: {
+            fillStyle: '#F35e66',
+            strokeStyle: 'black',
+            lineWidth: 1
+        }
+    });
+
+    World.add(engine.world, [ball]);
+    Engine.run(engine);
+    Render.run(render);
+});
